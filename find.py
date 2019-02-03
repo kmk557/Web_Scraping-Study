d1312905d314fc5eee54e7994d8b6698a1c69617
Vazirac Carlcox <32671162+kmk557@users.noreply.github.com>
2019-02-03 16:07:21 +0900

    파이썬 각종 정리

--- find.py
+++ find.py
@@ -1,8 +1,3 @@
-import re
-
-x = open('short.txt')
-#파일을 불러옵니다.
-for line in x:
-    line = line.rstrip()        #오른쪽에서부터 공백이 아닐때까지 공백값을 컷
-    if re.search('From:',line): #문장에서 '' 안의 값이 있는 지 확인한다. ( 대소문자 구분 )
-        print(line)
+import socket
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect( ('data.pr4e.org', 80) )

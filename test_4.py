2500ac22d2d8dc04baf662992128acf42f275acf
Vazirac Carlcox <32671162+kmk557@users.noreply.github.com>
2019-02-10 23:31:29 +0900

    ㅎ

--- test_4.py
+++ test_4.py
@@ -5,4 +5,4 @@ driver = webdriver.Chrome('D:/PycharmProjects/chromedriver')
 driver.implicitly_wait(3)
 #데이터 불러오는 시간 3초로 정한다.
 ## url에 접근한다.
-driver.get('https://naver.com')
+driver.get('https://comic.naver.com/webtoon/detail.nhn?titleId=715772&no=8')

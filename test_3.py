ab0869ceb887243712c352fce5417df10cdbcac1
Vazirac Carlcox <32671162+kmk557@users.noreply.github.com>
2019-02-14 00:19:33 +0900

    img to pdf & os module 학습

---
+++ test_3.py
@@ -0,0 +1,21 @@
+import os
+import re
+from PIL import Image
+
+webtoon_route = "D:\PycharmProjects\Web_Scraping-Study\화산전생 1화"
+list = webtoon_route.split('\\')
+# webtoon_name = re.findall("Pycharm", webtoon_route)
+
+# print(os.listdir(webtoon_route))
+img_list = [i for i in os.listdir(webtoon_route) if i.endswith(".jpg")]
+open_img_list = []
+pdf_name = list[-1]+".pdf"
+
+print(img_list)
+
+for img in img_list:
+    path = webtoon_route + "\\" + img
+    im = Image.open(path)
+    open_img_list.append(im)
+
+# open_img_list[0].save(pdf_name, "PDF", resolution = 100.0, save_all = True, append_images = open_img_list)
